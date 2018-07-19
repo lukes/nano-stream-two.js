a648fac8c8ef9f032be70f805afddc5e8b95c824
Luke Duncalfe <lduncalfe@eml.cc>
2018-07-19 19:27:37 +1200

    Call dispose() on children before removing self from scene

--- src/Group.js
+++ src/Group.js
@@ -30,8 +30,8 @@ export default class Group extends Two.Group {
   }
 
   dispose() {
-    two.remove(this).unbind('update', this.onUpdate);
     this.children.forEach(child => child.dispose());
+    two.remove(this).unbind('update', this.onUpdate);
   }
 
   onUpdate(/* frameCount */) {

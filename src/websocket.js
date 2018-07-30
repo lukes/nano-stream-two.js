5ec91af5a9893c1452059c3a43151cf180608559
Luke Duncalfe <lduncalfe@eml.cc>
2018-07-30 13:43:17 +1200

    Use reconnectingwebsocket library for websocket client

--- src/websocket.js
+++ src/websocket.js
@@ -1,5 +1,6 @@
+import ReconnectingWebSocket from 'reconnectingwebsocket';
 
-const socket = new WebSocket('ws://128.199.228.52:8080', 'protocolOne');
+const socket = new ReconnectingWebSocket('ws://128.199.228.52:8080', 'protocolOne');
 socket.onopen = () => {
   console.log('Connected');
 };
